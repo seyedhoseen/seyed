20cec2d6a64ccf2cbd888a4427d6a57aeb9247b2
seyedhoseen <133112993+seyedhoseen@users.noreply.github.com>
2023-07-25 02:02:31 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x6d17a0508d10d7fd7357c0b0d0ea524b6d346a23";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
